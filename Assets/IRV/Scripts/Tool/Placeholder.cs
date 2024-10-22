40ac98862855bea41e765a05ca9bdc51ec8a4398
Miquel Barbarà Garrido <miquel.barbara@salle.url.edu>
2024-10-22 19:05:43 +0200

    Workflow setup.

---
+++ Assets/IRV/Scripts/Tool/Placeholder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Placeholder : MonoBehaviour
+{
+    [SerializeField] private Step myStep;
+    private void OnTriggerEnter(Collider other)
+    {   
+        //Check if the step is the correct one
+        if (!WorkflowController.Instance.IsMyTurn(myStep)) return;
+        if (!WorkflowController.Instance.AllRequiredStepsDone()) return;
+        
+        if (other.gameObject.GetComponent<Accessories>())
+        {
+            other.gameObject.transform.position = gameObject.transform.position;
+            WorkflowController.Instance.Next();
+        }
+    }
+}
