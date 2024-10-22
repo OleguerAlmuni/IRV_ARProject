3409b77e20b10364bea59f4e3c445426df13415a
Miquel Barbarà Garrido <miquel.barbara@salle.url.edu>
2024-10-22 18:57:07 +0200

    First approach of the workflow.

---
+++ Assets/Scripts/WorkflowController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class WorkflowController:MonoBehaviour
+{
+        public static WorkflowController Instance;
+
+        public Instruction instruction;
+
+        public void Initialize(Instruction instruction)
+        {
+                this.instruction = instruction;
+                
+        }
+
+        private void Awake()
+        {
+                Instance = this;
+        }
+
+        public bool IsMyTurn(Step step)
+        {
+                if (step.index == instruction.actualStep.index)
+                {
+                        return true;
+                }
+
+                return false;
+        }
+
+        public void Next()
+        {
+                //verify if al the previous steps are done
+                instruction.NextStep();
+        }
+
+        public bool AllRequiredStepsDone()
+        {
+                foreach (int step in instruction.actualStep.stepsRequired)
+                {
+                        if (!instruction.stepsToFollow[step].isDone)
+                        {
+                                return false;
+                        }
+                }
+
+                return true;
+        }
+        
+
+}
