2542059c5b49886fdfe369a7cce84188aedd2df2
OleguerAlmuni <olegueralmuniorra@gmail.com>
2024-12-03 10:40:28 +0100

    Added LerpColor.cs to make an object blink if it needs to be used in the current step. Also added code in AssemblyComponent.cs to try the audio.

---
+++ Assets/IRV/Scripts/Tool/LerpColor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace IRV.Scripts.Tool
+{
+    public class LerpColor : MonoBehaviour
+    {
+        private MeshRenderer _meshRenderer;
+        private AssemblyComponent _assemblyComponent;
+        
+        [SerializeField] 
+        [Range(0f, 0.5f)] 
+        private float lerpSpeed;
+        
+        [SerializeField] 
+        private Color lerpColor;
+
+        private void Start()
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _assemblyComponent = GetComponent<AssemblyComponent>();
+        }
+        
+        private void Update()
+        {
+            if (WorkflowController.Instance.IsMyTurn(_assemblyComponent.getStep()))
+            {
+                _meshRenderer.material.color = Color.Lerp(_meshRenderer.material.color, lerpColor, lerpSpeed);
+            }
+        }
+    }
+}
